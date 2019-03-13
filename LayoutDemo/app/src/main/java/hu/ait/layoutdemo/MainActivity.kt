5476977cc3887798d22f09e924115d251c1a1e73
Peter Ekler <peter.ekler@gmail.com>
2019-03-13 18:12:04 +0100

    LinearLayout with ScrollView RelativeLayout ConstraintLayout demos, Login layout Special views: ImageView, AutoCompleteTextView

--- LayoutDemo/app/src/main/java/hu/ait/layoutdemo/MainActivity.kt
+++ LayoutDemo/app/src/main/java/hu/ait/layoutdemo/MainActivity.kt
@@ -2,11 +2,25 @@ package hu.ait.layoutdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.login_layout.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val cityNames = arrayOf("Budapest", "Bukarest", "New York", "New Delhi")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //setContentView(R.layout.linear_demo)
+        //setContentView(R.layout.constraint_demo)
+        setContentView(R.layout.login_layout)
+
+        val cityAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            cityNames
+        )
+        autoCompleteCity.setAdapter(cityAdapter)
     }
 }

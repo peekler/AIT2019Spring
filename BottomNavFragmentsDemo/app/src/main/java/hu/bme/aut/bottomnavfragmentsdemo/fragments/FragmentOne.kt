5e16aa0607104f8a7494c668fb0782f92fbfa211
Peter Ekler <peter.ekler@gmail.com>
2019-05-15 20:45:49 +0200

    BroadcastReceiver demo, Service demo, ContentProvider demo, Wear demo

--- BottomNavFragmentsDemo/app/src/main/java/hu/bme/aut/bottomnavfragmentsdemo/fragments/FragmentOne.kt
+++ BottomNavFragmentsDemo/app/src/main/java/hu/bme/aut/bottomnavfragmentsdemo/fragments/FragmentOne.kt
@@ -5,7 +5,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import hu.bme.aut.bottomnavfragmentsdemo.R
+import kotlinx.android.synthetic.main.fragment_one.view.*
 
 class FragmentOne: Fragment() {
 
@@ -15,6 +17,10 @@ class FragmentOne: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView=inflater.inflate(R.layout.fragment_one,container,false)
+
+        rootView.button.setOnClickListener {
+            Toast.makeText(activity, "DEMO", Toast.LENGTH_LONG).show()
+        }
 
         return rootView
     }
